9e3676ae9186153124ac1369625a1c47ce2170f7
Chenj <cjc28082@163.com>
2025-03-06 19:52:00 +0800

    Site updated: 2025-03-06 19:51:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/06/hello-world/"];function toRandomPost(){
+var posts=["2025/03/06/hello-world/","2025/03/06/first-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
