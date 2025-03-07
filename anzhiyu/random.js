13b3fc59d43f52e52681a7657e1e421eb3e4c87c
Chenj <cjc28082@163.com>
2025-03-07 22:37:58 +0800

    Site updated: 2025-03-07 22:37:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/06/hello-world/","2024/03/06/first-blog/"];function toRandomPost(){
+var posts=["2025/03/06/hello-world/","2024/03/06/first-blog/","2024/03/07/近期总结与计划/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
